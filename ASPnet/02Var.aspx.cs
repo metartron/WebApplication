5d3afb553d57204084e5972b232d7f5a7b7d1802
C50120@VIEW.LOCAL <C50120@VIEW.LOCAL>
2019-01-30 10:10:53 +0800

    加入專案檔案。

---
+++ ASPnet/02Var.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPnet
+{
+    public partial class _02Var : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<h1>Hellow World!!</h1>");
+            //字串
+            string var;
+            var = "Hellow World!!";
+            var = "abcde";
+            Response.Write(var);
+            Response.Write("<br>");
+            //布林值
+            bool gender = true;
+
+            //整數
+            int a = 9999945; //32bit整數
+            long b = 23343433; //64bit整數
+            short c = 444;//16bit整數
+            byte d = 5;//8bit正整數
+
+            //浮點數
+            float f = 123.123f;
+            Response.Write(f);
+            double g = 123.123;
+
+            //變數命名原則
+            //不可以用保留字
+            //開頭必須是英文字母或底線
+            //同一支程式區塊內,變數名稱不可重複,大小寫視為不同
+
+            //變數命名原則
+            //使用有意義的名稱
+            //使用匈牙利命名法
+            string strStudentNo = "12345679";
+            int intNumber = 123;
+        }
+    }
+}
---
+++ ASPnet/04practice-1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPnet
+{
+    public partial class _04practice_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /********************************************************
+             * 宣告變數a為整數，值為42，宣告變數b為浮點數，值2.5，  *
+             *將兩值分別做加、減、乘、除及取餘數之運算，並輸出其結果*
+             ********************************************************/
+            int a = 42;
+            float b = 2.5f;
+
+            Response.Write(a + "+" + b + "=" + (a + b));
+            Response.Write("<br />");
+            Response.Write(a + "-" + b + "=" + (a - b));
+            Response.Write("<br />");
+            Response.Write(a + "*" + b + "=" + a * b);
+            Response.Write("<br />");
+            Response.Write(a + "/" + b + "=" + a / b);
+            Response.Write("<br />");
+            Response.Write(a + "%" + b + "=" + a % b);
+            Response.Write("<hr />");
+
+
+            /*****************************************
+             * 撰寫一個將攝氏溫度轉換為華氏溫度的程式*
+             * 攝氏溫度的值直接在程式中給定即可      *
+             *(華氏＝攝氏 * 9 / 5 + 32)              *
+             *****************************************/
+
+            float c = 33.5f;
+            Response.Write("攝氏"+c+"度等於華氏" + (c * 9 / 5 + 32) +"度");
+            Response.Write("<hr />");
+
+            /***********************************
+            * 設有兩個變數X與Y，其值為任何整數 *
+            * 試寫交換X與Y的值的程式           *
+            *(例X=3,Y=5,執行完您的程式後X=5,Y=3*
+            ************************************/
+    
+            //int X = 969, Y = 524;
+
+            //int iChange = X;
+            //X = Y;
+            //Y = iChange;
+
+            //Response.Write("X=" + X + ", Y=" + Y);
+
+            /************************************
+           * 設有兩個變數X與Y，其值為任何整數 *
+           * 試寫在不另宣告其他變數的情況下   *
+           * 交換X與Y的值的程式               *
+           *(例X=3,Y=5,執行完您的程式後X=5,Y=3*
+           ************************************/
+
+            int X = 3, Y = 5;
+            //X = X + Y; //3+5=8
+            //Y = X - Y; //8-5=3
+            //X = X - Y;//8-3=5
+
+            X = X ^ Y;
+            Y = X ^ Y;
+            X = X ^ Y;
+
+            Response.Write("X=" + X + ", Y=" + Y);
+        }
+    }
+}
