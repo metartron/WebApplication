f7732a2ba0ae16cb20b18b7323ed3e3c88a6cb7d
Meta Jheng <sailoruranu@hotmail.com>
2019-02-01 11:22:11 +0800

    20190201003

--- Homework/HW3.aspx.cs
+++ Homework/HW3.aspx.cs
@@ -9,19 +9,64 @@ namespace Homework
 {
     public partial class HW3 : System.Web.UI.Page
     {
-        string[] sImg = {"1.gif","2.gif","3.gif", "4.gif" , "5.gif" , "6.gif" , "7.gif" , "8.gif" , "9.gif" , "10.gif" , "11.gif" , "12.gif" +
-                          "13.gif","14.gif","15.gif","16.gif","17.gif","18.gif","19.gif","20.gif","21.gif","22.gif","23.gif","24.gif" +
-                          "25.gif","26.gif","27.gif","28.gif","29.gif","30.gif","31.gif","32.gif","33.gif","34.gif","35.gif","36.gif" +
-                          "37.gif","38.gif","39.gif","40.gif","41.gif","42.gif","43.gif","44.gif","45.gif","46.gif","47.gif","48.gif" +
-                          "49.gif","50.gif","51.gif","52.gif"};
-
         protected void Page_Load(object sender, EventArgs e)
         {
 
             //試寫一撲克牌發牌程式，將52張牌發給四玩家，每家共13張，並利用poker_img資料夾裡的素材來顯示撲克牌。
             //(ps.每次發牌均需為不同結果)
+            int[] randomNum = new int[52];
+            int iLen = randomNum.Length;
+            for (int i = 0; i < iLen ; i++)
+            {
+                randomNum[i] = (i + 1);
+                Response.Write("<img src='poker_img/" + randomNum[i] + ".gif' />");
+            }
+            Response.Write("<hr>");
+            ///////////////////////////////////////////////////////////////////////////
+            Random rd = new Random();
+            int ir;
+            int tmp;
+            for (int i = 0; i < iLen ; i++)
+            {
+                ir = rd.Next(i, iLen);
+                if (i == ir) continue;
+                tmp = randomNum[i];
+                randomNum[i] = randomNum[ir];
+                randomNum[ir] = tmp;
+            }
+            for (int i = 0; i < iLen ; i++)
+            {
+                Response.Write("<img src='poker_img/" + randomNum[i] + ".gif' />");
+            }
+            Response.Write("<hr>");
+            ///////////////////////////////////////////////////////////////////////////
+            int irow = 0;
+            int icol = 0;
+            int[,] sPoker = new int[4, 13];
 
-
+            for (int i = 0; i < iLen ; i++)
+            {
+                if (irow == sPoker.GetLongLength(0))
+                {
+                    irow = 0;
+                    icol++;
+                }
+                sPoker[irow, icol] = randomNum[i];
+                irow++;
+                if (icol == sPoker.GetLongLength(1))
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < sPoker.GetLongLength(0); i++)
+            {
+                Response.Write((i+1) +" : ");
+                for (int j = 0; j < sPoker.GetLongLength(1); j++)
+                {
+                    Response.Write("<img src='poker_img/" + sPoker[i,j] + ".gif' />");
+                }
+                Response.Write("<br>");
+            }
         }
     }
 }
