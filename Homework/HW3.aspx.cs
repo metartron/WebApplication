ee10d165a3d4096da7867ffb92a81e2278ba229d
Meta Jheng <sailoruranu@hotmail.com>
2019-02-01 08:46:15 +0800

    20190201002

---
+++ Homework/HW3.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Homework
+{
+    public partial class HW3 : System.Web.UI.Page
+    {
+        string[] sImg = {"1.gif","2.gif","3.gif", "4.gif" , "5.gif" , "6.gif" , "7.gif" , "8.gif" , "9.gif" , "10.gif" , "11.gif" , "12.gif" +
+                          "13.gif","14.gif","15.gif","16.gif","17.gif","18.gif","19.gif","20.gif","21.gif","22.gif","23.gif","24.gif" +
+                          "25.gif","26.gif","27.gif","28.gif","29.gif","30.gif","31.gif","32.gif","33.gif","34.gif","35.gif","36.gif" +
+                          "37.gif","38.gif","39.gif","40.gif","41.gif","42.gif","43.gif","44.gif","45.gif","46.gif","47.gif","48.gif" +
+                          "49.gif","50.gif","51.gif","52.gif"};
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //試寫一撲克牌發牌程式，將52張牌發給四玩家，每家共13張，並利用poker_img資料夾裡的素材來顯示撲克牌。
+            //(ps.每次發牌均需為不同結果)
+
+
+        }
+    }
+}
